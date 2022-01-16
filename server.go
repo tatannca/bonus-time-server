6f4de682cd2dab05f09627a5e2d77d558a456f99
tateno <gd28.c.tateno@gmail.com>
2022-01-16 20:56:45 +0900

    Echoでテスト用レスポンス作成

--- server.go
+++ server.go
@@ -4,28 +4,26 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+	}))
+
+	type Message struct {
+		Public string `json:"message"`
+	}
+	
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		m := &Message{
+			Public: "Hello Public World.",
+		}
+		return c.JSON(http.StatusOK, m)
 	})
 	e.Logger.Fatal(e.Start(":5000"))
 }
-
-// func CORSMiddleware() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 			c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
-// 			c.Header("Access-Control-Allow-Credentials", "true")
-// 			c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-// 			c.Header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
-// 			c.Next()
-// 	}
-// }
-
-// func public(c *gin.Context) {
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message": "hello public!",
-// 	})
-// }
